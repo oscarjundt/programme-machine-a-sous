3fd3c7bc62a46beee0a69b4c81d48cb7dbe7531e
oscarjundt <oscar.jundt-schmitter@laposte.net>
2024-01-10 17:04:40 +0100

    updat

--- tpnote/src/main/java/org/example/Column.java
+++ tpnote/src/main/java/org/example/Column.java
@@ -7,9 +7,19 @@ public class Column {
     private List<String> c2;
     private List<String> c3;
     private final String[][] JsonColumn;
+    // Le code `public Column(String[][] JsonColumn){ this.JsonColumn = JsonColumn; }` est un constructeur pour la classe
+    // `Column`. Il prend un tableau 2D de chaînes `JsonColumn` comme paramètre et l'assigne à la variable d'instance
+    // `JsonColumn` de la classe. Cela permet d'accéder et d'utiliser les données `JsonColumn` dans les méthodes de la
+    // classe `Column`.
     public Column(String[][] JsonColumn){
         this.JsonColumn = JsonColumn;
     }
+    /**
+     * La fonction "loadColumn" renvoie une liste de listes contenant des éléments sélectionnés aléatoirement dans trois
+     * colonnes différentes.
+     *
+     * @return La méthode loadColumn() renvoie une liste de listes de chaînes.
+     */
     public List<List<String>> loadColumn(){
         List<List<String>> colomun = new ArrayList<>();
         this.c1  = (dataColumnRand(this.JsonColumn[0], new Random().nextInt(this.JsonColumn[0].length)));
@@ -20,6 +30,12 @@ public class Column {
         colomun.add(c3);
         return colomun;
     }
+    /**
+     * La fonction vérifie si une colonne spécifiée dans une grille 2D contient la même valeur dans toutes ses cellules.
+     *
+     * @param nbJeton Le paramètre nbJeton représente le numéro de colonne à vérifier. Il peut avoir les valeurs 1, 2 ou 3.
+     * @return La méthode renvoie une valeur booléenne, qui indique si une certaine condition est vraie ou fausse.
+     */
     public boolean checkColumn(int nbJeton){
         boolean statut = false;
         if(nbJeton==1){
@@ -39,6 +55,14 @@ public class Column {
         }
         return statut;
     }
+    /**
+     * La fonction prend un tableau de chaînes et un index, et renvoie une ArrayList contenant trois éléments du tableau à
+     * partir de l'index donné.
+     *
+     * @param col Un tableau de chaînes représentant une colonne de données.
+     * @param index Le paramètre index est un entier qui représente l'index de départ dans le tableau col.
+     * @return La méthode renvoie une ArrayList de chaînes.
+     */
     private ArrayList<String> dataColumnRand(String[] col, int index){
         ArrayList<String> colonne = new ArrayList<>();
         for(int i=0;i<3;i++){
--- tpnote/src/main/java/org/example/Machine.java
+++ tpnote/src/main/java/org/example/Machine.java
@@ -9,6 +9,7 @@ import java.util.Random;
 public class Machine {
     private Column colonne;
     //private String[][] Matrice;
+    // Le constructeur `public Machine()` initialise une nouvelle instance de la classe `Machine`.
     public Machine(){
         Gson gson = new Gson();
         this.colonne =  new Column(gson.fromJson("[\n" +
@@ -18,14 +19,31 @@ public class Machine {
                 "]",String[][].class));
 
     }
+    /**
+     * La fonction start() imprime le résultat du chargement d'une machine et continue de le faire jusqu'à ce qu'une
+     * certaine condition soit remplie.
+     */
     public void start(){
         //System.out.println(Arrays.deepToString(loadMachine()));
         this.afficheResult(this.loadMachine());
         while (!this.colonne.checkColumn(2)) this.afficheResult(this.loadMachine());
     }
+    /**
+     * La fonction "loadMachine" renvoie une liste de listes de chaînes, obtenue en appelant la méthode "loadColumn" de
+     * l'objet "colonne".
+     *
+     * @return Une liste de listes de chaînes est renvoyée.
+     */
     private List<List<String>> loadMachine(){
         return this.colonne.loadColumn();
     }
+    /**
+     * La fonction afficheResult imprime les éléments d'une liste 2D sous forme de tableau puis vérifie une colonne
+     * spécifique.
+     *
+     * @param columnRand Le paramètre `columnRand` est une liste de listes de chaînes. Chaque liste interne représente une
+     * colonne de données et la liste externe représente une collection de colonnes.
+     */
     private void afficheResult(List<List<String >> columnRand){
         for(int i=0;i<columnRand.size();i++){
             for (int j=0;j<columnRand.get(i).size();j++){
